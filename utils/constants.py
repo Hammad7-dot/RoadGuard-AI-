54b1fec4e0bce245208a2e22393306a484bb0295
Muhammad Hammad <mhammadfazil14@gmail.com>
2026-07-23 14:54:54 +0500

    Update DAMAGE_CLASSES to match the 4-class RDD2022 model actually deployed

--- utils/constants.py
+++ utils/constants.py
@@ -7,11 +7,8 @@ MODEL_NAME = "YOLOv8"
 MODEL_PATH = "ai/models/best.pt"
 
 DAMAGE_CLASSES = [
-    "Pothole",
     "Longitudinal Crack",
     "Transverse Crack",
     "Alligator Crack",
-    "Manhole Cover",
-    "Patch",
-    "Road Marking Damage",
+    "Pothole",
 ]
